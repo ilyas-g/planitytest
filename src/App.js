308c837d1a92026a1e6b615be53f8072d53ede40
ilyas-g <ilyas15069i@gmail.com>
2022-10-20 15:55:55 +0200

    Objects of the inputs's array sorted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
+import inputs from './input.json';
+
 import './App.css';
 
 function App() {
+
+  const sortable = (a, b) => {
+    if (a.start < b.start) { return -1; }
+    if (a.start > b.start) { return 1; }
+    return 0;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        {inputs.sort(sortable).map(input => {
+          return (
+            <div className="box">
+              {input.id}
+            </div>
+          );
+        })}
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.

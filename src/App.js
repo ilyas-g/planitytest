89939efffd38c84599990012885ae80613e4fbc4
ilyas-g <ilyas15069i@gmail.com>
2022-10-23 17:39:20 +0200

    Ajout du CSS d'une box

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import inputs from './input.json';
 
 import './App.css';
@@ -11,30 +11,47 @@ function App() {
     return 0;
   };
 
+  let innerWidth = window.innerWidth;
+  let innerHeight = window.innerHeight;
+
+
+
+  function handleResize() {
+    console.log('resized to: ', window.innerWidth, 'x', window.innerHeight);
+  }
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        {inputs.sort(sortable).map(input => {
-          return (
-            <div className="box">
-              {input.id}
-            </div>
-          );
-        })}
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div className='box'>
+        1
+      </div> */}
+
+      {inputs.sort(sortable).map((input, index) => {
+
+        let boxStyle = {
+          backgroundColor: "#4a5771",
+          color: "#ffffff",
+          border: "1px solid #ffffff",
+          marginBottom: '20px',
+          width: index === 0 ? innerWidth : innerWidth / (index + 1),
+          // width: innerWidth / (index + 1),
+          // width: innerWidth,
+          height: innerHeight,
+        };
+
+        // boxStyle.width = innerWidth / index;
+        // console.log(boxStyle.width);
+        return (
+          <div style={boxStyle} key={index}>
+            <p>{boxStyle.width}</p>
+            {input.id} - {index}
+          </div>
+        );
+      })}
     </div>
   );
 }
